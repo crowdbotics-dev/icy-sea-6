d590d11b82b1a6ea4ccbb3f04efe4790512b71a8
Crowdbotics <engineering@crowdbotics.com>
2023-03-22 15:58:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-sea-6",
+  url: "https://icy-sea-6.botics.co",
+  api: "https://icy-sea-6.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
